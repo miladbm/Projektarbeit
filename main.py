d3795c90234b2909d153fe544cf8aee9484ee091
miladbm <100361838+miladbm@users.noreply.github.com>
2022-04-14 09:50:29 +0200

    Created gitignore

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
-from flask import request
-import random
 
-app = Flask(__name__)
+app = Flask("Projektarbeit")
+
 
 @app.route("/")
-def hello():
+def start():
+    name = "Mirjam"
+    cards = [
+        {"titel": "Card 0", "inhalt": "Card 1"},
+        {"titel": "Card 1", "inhalt": "Card 2"},
+        {"titel": "Card 2", "inhalt": "Card 3"},
+        {"titel": "Card 2", "inhalt": "Card 4"}
+    ]
+    return render_template("index.html", name=name, cards=cards)
 
-    names = ['Fabian', "Wolfgang", "Michael", "Cristina", "Meret"]
-    name_choice = random.choice(names)
-    about_link = url_for("about")
-    return render_template("index.html", name=name_choice, link=about_link)
-
-
-@app.route("/about")
-def about():
-    return "About test"
-
-
-@app.route('/hello/<name2>')  # /hello/Fabian
-def begruessung(name2):
-    return "Hallo " + name2 + "!"
-
-
-@app.route("/form")
-def form():
-    if request.method.lower() == "get":
-        return render_template("formular.html")
-    if request.method.lower() =="post"
-        name = request.form["vorname"]
-        return name
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)

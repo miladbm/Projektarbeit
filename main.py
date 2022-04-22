58b25f5c753a38735e9ae82b9b37c5449057ccac
miladbm <100361838+miladbm@users.noreply.github.com>
2022-04-22 15:41:15 +0200

    Created gitignore

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import daten
 
 app = Flask("Projektarbeit")
 
 
+#Verlinkung auf Hauptseite
 @app.route("/")
 def start():
     name = "Mirjam"
@@ -15,6 +18,23 @@ def start():
     ]
     return render_template("index.html", name=name, cards=cards)
 
+#Verlinkung auf Formular
+@app.route("/formular/", methods=['GET', 'POST'])
+def formular():
+    if request.method == 'POST':
+        #ziel_person = request.form['vorname']
+        #rueckgabe_string = "Hello " + ziel_person + "!"
+        #return rueckgabe_string
+        return render_template("formular.html")
+    else:
+        return render_template("formular.html")
+
+
+@app.route("/formular/daten_einkaufsliste.json")
+def speichern(aktivitaet):
+    zeitpunkt, aktivitaet = daten.aktivitaet_speichern(aktivitaet)
+
+    return "Gespeichert: " + aktivitaet + " um " + str(zeitpunkt)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)

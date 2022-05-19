2423cb0dad18019cdc6109e8805cb2592ff901f4
miladbm <100361838+miladbm@users.noreply.github.com>
2022-05-19 17:31:52 +0200

    Created gitignore

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ def start():
 def formular():
     if request.method == 'POST':
         data = request.form
-        vorname = data["name"]
-        gekauft = data["was"]
-        ausgegeben = data["betrag"]
+        name = data["name"]
+        was = data["was"]
+        betrag = data["betrag"]
         zeitpunkt = datetime.now()
 
 
         daten = daten_laden()
 
-        my_dict = {"Vorname": vorname, "Was": gekauft, "Ausgegeben": ausgegeben}
+        my_dict = {"Name": name, "Was": was, "Betrag": betrag}
         daten.update({str(zeitpunkt): my_dict})
 
 
@@ -50,7 +50,55 @@ def ausgaben():
     with open("eingaben_nutzer.json") as open_file:
         json_as_string = open_file.read()
         my_read_dict = loads(json_as_string)
-        return render_template("ausgaben.html")
+
+        return render_template("ausgaben.html", eingabe_nutzer=my_read_dict)
+
+@app.route("/berechnung/")
+def berechnung():
+    with open("eingaben_nutzer.json") as open_file:
+        json_as_string = open_file.read()
+        my_read_dict = loads(json_as_string)
+
+    summe_mirjam = 0
+    summe_luca = 0
+    summe_sarina = 0
+
+    # namen = ['Mirjam', 'Luca', 'Sarina']
+    # ausgaben = {}
+    #
+    # for name in namen:
+    #     for key, value in my_read_dict.items():
+    #         if value['Name'] == name:
+    #             try:
+    #                 if name in ausgaben.keys():
+    #                     ausgaben[name]= ausgaben.get(name,0) + value['Betrag']
+    #                 else:
+    #                     ausgaben[]
+
+
+
+    for key, value in my_read_dict.items():
+        if value['Name']=='Mirjam':
+            try:
+                summe_mirjam += float(value['Betrag'])
+
+            except:
+                continue
+
+        elif value['Name']=='Luca':
+            try:
+                summe_luca += float(value['Betrag'])
+            except:
+                continue
+
+        elif value['Name']=='Sarina':
+            try:
+                summe_sarina += float(value['Betrag'])
+            except:
+                continue
+
+    return render_template("berechnung.html", summe_mirjam=summe_mirjam, summe_luca=summe_luca, summe_sarina=summe_sarina)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)

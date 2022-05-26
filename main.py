01ab50eb922c1c27c771ffd31742123dc8685bd7
miladbm <100361838+miladbm@users.noreply.github.com>
2022-05-26 20:10:39 +0200

    Created gitignore

--- main.py
+++ main.py
@@ -4,27 +4,23 @@ from flask import request
 import json
 from daten import daten_laden
 from datetime import datetime
-from json import loads, dumps
+from json import loads
+import plotly.express as px
+from plotly.offline import plot
 import os
-import os.path
 from os.path import exists
+
 
 app = Flask("Projektarbeit")
 
 
-#Verlinkung auf Hauptseite
+# Verlinkung auf Hauptseite
 @app.route("/")
 def start():
-    name = "Mirjam"
-    cards = [
-        {"titel": "Card 0", "inhalt": "Card 1"},
-        {"titel": "Card 1", "inhalt": "Card 2"},
-        {"titel": "Card 2", "inhalt": "Card 3"},
-        {"titel": "Card 2", "inhalt": "Card 4"}
-    ]
-    return render_template("index.html", name=name, cards=cards)
+    return render_template("index.html")
 
-#Verlinkung auf Formular
+
+# Verlinkung auf Formular
 @app.route("/formular/", methods=['GET', 'POST'])
 def formular():
     if request.method == 'POST':
@@ -34,12 +30,10 @@ def formular():
         betrag = data["betrag"]
         zeitpunkt = datetime.now()
 
-
         daten = daten_laden()
 
         my_dict = {"Name": name, "Was": was, "Betrag": betrag}
         daten.update({str(zeitpunkt): my_dict})
-
 
         with open("eingaben_nutzer.json", "w") as open_file:
             json.dump(daten, open_file, default=str)
@@ -47,67 +41,66 @@ def formular():
     else:
         return render_template("formular.html")
 
+
 @app.route("/ausgaben/")
 def ausgaben():
-    with open("eingaben_nutzer.json") as open_file:
-        json_as_string = open_file.read()
-        my_read_dict = loads(json_as_string)
+    if exists("eingaben_nutzer.json"):
+        with open("eingaben_nutzer.json") as open_file:
+            json_as_string = open_file.read()
+            my_read_dict = loads(json_as_string)
 
-    return render_template("ausgaben.html", eingabe_nutzer=my_read_dict)
+        return render_template("ausgaben.html", eingabe_nutzer=my_read_dict)
+    else:
+        return render_template("daten_loeschen.html")
 
-@app.route("/delete/")
-def delete():
-    with open("eingaben_nutzer.json", "w") as open_file:
-        json.dump({})
-    """with open("eingaben_nutzer.json", "w") as open_file:
-        dict_leer=open_file.clear()
-    return render_template("ausgaben.html", dict_leer=dict_leer)
-"""
+
+@app.route("/ausgaben/daten_loeschen")
+def daten_loeschen():
+    if exists("eingaben_nutzer.json"):
+        os.remove("eingaben_nutzer.json")
+        return render_template("daten_loeschen.html")
+    else:
+        return render_template("daten_loeschen.html")
+
+
 @app.route("/berechnung/")
 def berechnung():
-    with open("eingaben_nutzer.json") as open_file:
-        json_as_string = open_file.read()
-        my_read_dict = loads(json_as_string)
+    if exists("eingaben_nutzer.json"):
+        with open("eingaben_nutzer.json") as open_file:
+            json_as_string = open_file.read()
+            my_read_dict = loads(json_as_string)
 
-    summe_mirjam = 0
-    summe_luca = 0
-    summe_sarina = 0
+        summe_mirjam = 0
+        summe_luca = 0
+        summe_sarina = 0
 
-    # namen = ['Mirjam', 'Luca', 'Sarina']
-    # ausgaben = {}
-    #
-    # for name in namen:
-    #     for key, value in my_read_dict.items():
-    #         if value['Name'] == name:
-    #             try:
-    #                 if name in ausgaben.keys():
-    #                     ausgaben[name]= ausgaben.get(name,0) + value['Betrag']
-    #                 else:
-    #                     ausgaben[]
+        for key, value in my_read_dict.items():
+            if value['Name'] == 'Mirjam':
+                try:
+                    summe_mirjam += float(value['Betrag'])
 
+                except:
+                    continue
 
+            elif value['Name'] == 'Luca':
+                try:
+                    summe_luca += float(value['Betrag'])
+                except:
+                    continue
 
-    for key, value in my_read_dict.items():
-        if value['Name']=='Mirjam':
-            try:
-                summe_mirjam += float(value['Betrag'])
+            elif value['Name'] == 'Sarina':
+                try:
+                    summe_sarina += float(value['Betrag'])
+                except:
+                    continue
 
-            except:
-                continue
+        fig = px.bar(x=["Luca", "Mirjam", "Sarina"], y=[summe_luca, summe_mirjam, summe_sarina])
+        div = plot(fig, output_type="div")
+        fig.show()
 
-        elif value['Name']=='Luca':
-            try:
-                summe_luca += float(value['Betrag'])
-            except:
-                continue
-
-        elif value['Name']=='Sarina':
-            try:
-                summe_sarina += float(value['Betrag'])
-            except:
-                continue
-
-    return render_template("berechnung.html", summe_mirjam=summe_mirjam, summe_luca=summe_luca, summe_sarina=summe_sarina)
+        return render_template("berechnung.html", summe_mirjam=summe_mirjam, summe_luca=summe_luca, summe_sarina=summe_sarina, fig_div=div)
+    else:
+        return render_template("daten_loeschen.html")
 
 
 if __name__ == "__main__":
